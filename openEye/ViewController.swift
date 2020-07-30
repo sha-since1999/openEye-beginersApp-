cd1e3d5e451d05e3b033ab7d7e319948c5f8360a
Rohit sahu <rohits8853@gmail.com>
2020-07-30 17:22:38 +0530

    basics implementations

---
+++ openEye/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  openEye
+//
+//  Created by Rohit sahu on 19/07/20.
+//  Copyright © 2020 Rohit sahu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
